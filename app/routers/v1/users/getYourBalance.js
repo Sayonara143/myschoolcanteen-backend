d8f6c0bc4c94b3d05204bfd3c66bf2c783f8e294
Sayonara143 <sasha-bykov-1997@inbox.ru>
2020-04-27 21:02:05 +0500

    Io

--- app/routers/v1/users/getYourBalance.js
+++ app/routers/v1/users/getYourBalance.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', async (req,res) => {
     const user = req.user;
-    let object = {balance: user.balance, name: user.name, surname: user.surname}
+    let object = {balance: user.balance, name: user.name, surname: user.surname, path: user.path}
     res.json(object);
 });
 
