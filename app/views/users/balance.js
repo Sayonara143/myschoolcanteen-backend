f547397202b7b58838462949dbcf9e8ff6f28d4a
Sayonara143 <sasha-bykov-1997@inbox.ru>
2020-05-23 21:18:30 +0500

    version23-05-20

--- app/views/users/balance.js
+++ app/views/users/balance.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/', async function(req, res){
 
-    await  res.sendfile("app/public/views/balance/index.html")
+    await  res.sendfile("app/public/views/users/balance/index.html")
 })
 
 export default router
--- app/views/users/settings.js
+++ app/views/users/settings.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/', async function(req, res){
 
-    await  res.sendfile("app/public/views/settings/index.html")
+    await  res.sendfile("app/public/views/users/settings/index.html")
 })
 
 export default router
