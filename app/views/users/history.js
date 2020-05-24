876a192a3e6af1bdd76210c6a3767fbf117e6323
Sayonara143 <sasha-bykov-1997@inbox.ru>
2020-05-24 09:51:52 +0500

    version24-05-2020

--- app/views/users/history.js
+++ app/views/users/history.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/', async (req, res) => {
 
-    await res.sendfile("app/public/views/users//history/index.html")
+    await res.sendfile("/app/public/views/users//history/index.html")
 })
 
 export default router
--- app/views/users/myEat.js
+++ app/views/users/myEat.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/', async function(req, res){
 
-    await  res.sendfile("app/public/views/users/myEat/index.html")
+    await  res.sendfile("/app/public/views/users/myEat/index.html")
 })
 
 export default router
