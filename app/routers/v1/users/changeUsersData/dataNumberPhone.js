97b2f7ab7111e5ab8a4cc592b20a741f842c964a
Sayonara143 <sasha-bykov-1997@inbox.ru>
2020-06-02 19:16:05 +0500

    verion 02.06.2020 19:15

--- app/routers/v1/users/changeUsersData/dataNumberPhone.js
+++ app/routers/v1/users/changeUsersData/dataNumberPhone.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import * as UsersModelAPI from '../../../models/usersModel'
-import * as AccessTokenAPI from '../../../models/accessTokenUsers'
+import * as UsersModelAPI from '../../../../models/usersModel'
+import * as AccessTokenAPI from '../../../../models/accessTokenUsers'
 const router = express.Router();
 
 
