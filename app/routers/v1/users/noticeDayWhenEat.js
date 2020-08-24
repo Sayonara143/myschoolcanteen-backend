8440b365c9d208e2dc84a01f9dfb29f6c5d20e36
Sayonara143 <sasha-bykov-1997@inbox.ru>
2020-08-24 16:52:27 +0500

    16:52  24.08.2020

--- app/routers/v1/users/noticeDayWhenEat.js
+++ app/routers/v1/users/noticeDayWhenEat.js
@@ -7,18 +7,37 @@ const router = express.Router();
 
 let newNoticeData;
 router.post('/', async(req, res) => {
+    let d = new Date;
+    d.setHours(12)
     const user = req.user;
     const data = req.body;
     if (data.flag === null || data.date === null) {
         res.json("empty data")
     } else {
         try {
-          if (data.flag === true) {
-            const food = await CalendarFoodModelAPI.findCalendarFoodByDateAndAdminAndTicket(data.date, user.admin, user.ticket)
-            if (food.summa > user.balance) {
-              res.sendStatus(501)
-            } else{
-              const uBalance = user.balance - food.summa
+          console.log(data.date + '----' + d)
+          if (data.date > d) {
+            if (data.flag === true) {
+              const food = await CalendarFoodModelAPI.findCalendarFoodByDateAndAdminAndTicket(data.date, user.admin, user.ticket)
+              if (food.summa > user.balance) {
+                res.sendStatus(501)
+              } else{
+                const uBalance = user.balance - food.summa
+                newNoticeData = {
+                  user: {
+                    name: user.name,
+                    surname: user.surname,
+                    patronymic: user.patronymic,
+                    login: user.login,
+                    ticket: user.ticket,
+                  },
+                  adminLogin: user.admin,
+                  flag: data.flag,
+                  date: data.date
+                }
+                await UserApi.UpdateUsersBalance(user.login, uBalance)
+              }
+            } else {
               newNoticeData = {
                 user: {
                   name: user.name,
@@ -31,24 +50,13 @@ router.post('/', async(req, res) => {
                 flag: data.flag,
                 date: data.date
               }
-              await UserApi.UpdateUsersBalance(user.login, uBalance)
             }
+            await NoticeApi.createNotice(newNoticeData)
+            res.sendStatus(200);
           } else {
-            newNoticeData = {
-              user: {
-                name: user.name,
-                surname: user.surname,
-                patronymic: user.patronymic,
-                login: user.login,
-                ticket: user.ticket,
-              },
-              adminLogin: user.admin,
-              flag: data.flag,
-              date: data.date
-            }
+            res.sendStatus(403)
           }
-          await NoticeApi.createNotice(newNoticeData)
-          res.sendStatus(200);
+          
         } catch (error) {
             res.sendStatus(500);
             console.log(error.message);
